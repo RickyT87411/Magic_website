7a09d0ce5c3416309865a2c8f3e14ff3df1f016d
RickyT87411 <jamescook215@proton.me>
2023-06-22 06:37:50 -0700

    create slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
-console.log('Hello World!');
+import Slider from "./modules/sliders";
+window.addEventListener("DOMContentLoaded", () => {
+  const slider = new Slider(".page", ".next");
+  slider.render();
+});
---
+++ src/js/modules/sliders.js
@@ -0,0 +1,38 @@
+export default class Slider {
+  constructor(page, btns) {
+    this.page = document.querySelector(page);
+    this.slides = this.page.children;
+    this.btns = document.querySelectorAll(btns);
+    this.slideIndex = 1;
+  }
+
+  showSlides(n) {
+    if (n > this.slides.length) {
+      this.slideIndex = 1;
+    }
+
+    if (n < 1) {
+      this.slideIndex = this.slides.length; //равен последнему элементу
+    }
+
+    this.slides.forEach((slide) => {
+      slide.style.display = "none";
+    });
+    this.slides[this.slideIndex - 1].style.display = "flex";
+  }
+
+  plusSlides(n) {
+    this.showSlides((this.slideIndex += n)); //контроль передвижения слайдов
+  }
+
+  render() {
+    console.log(this.page, this.slides);
+
+    this.btns.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        this.plusSlides(1); // так как стрелка пока одна
+      });
+    });
+    this.showSlides(this.slideIndex); //обращение к методу внутри экземляра класса
+  }
+}
